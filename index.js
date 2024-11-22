078df88d4701b9bd375b1cd9997887eb7deb9a11
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2024-11-22 23:41:56 +0200

    Виконано третю частину роботи. Виконано документування сервісу.

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
 const upload = multer();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API Documentation',
+        version: '1.0.0',
+        description: 'API Documentation for note service'
+      },
+    },
+    apis: ['index.js'],
+  };
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 program
   .option('-h, --host <type>', 'server host')
@@ -41,11 +57,34 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.listen(option.port, option.host, () => {
     console.log(`Server is running on http://${option.host}:${option.port}`);
 });
 
+/**
+ * @swagger
+ * /notes/{noteName}:
+ *   get:
+ *     summary: Get the content of a note
+ *     parameters:
+ *       - in: path
+ *         name: noteName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the note
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: Note not found
+ */
 app.get('/notes/:noteName', (req, res) => {
     const notePath = path.join(option.cache, req.params.noteName);
 
@@ -57,6 +96,36 @@ app.get('/notes/:noteName', (req, res) => {
     res.send(noteText);
 });
 
+/**
+ * @swagger
+ * /notes/{noteName}:
+ *   put:
+ *     summary: Update the content of a note
+ *     parameters:
+ *       - in: path
+ *         name: noteName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the note
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               text:
+ *                 type: string
+ *                 description: New content for the note
+ *     responses:
+ *       200:
+ *         description: Note updated successfully
+ *       400:
+ *         description: Bad request (missing text)
+ *       404:
+ *         description: Note not found
+ */
 app.put('/notes/:noteName', (req, res) => {
     const notePath = path.join(option.cache, req.params.noteName);
 
@@ -71,6 +140,24 @@ app.put('/notes/:noteName', (req, res) => {
     res.send('Note updated');
 });
 
+/**
+ * @swagger
+ * /notes/{noteName}:
+ *   delete:
+ *     summary: Delete a note
+ *     parameters:
+ *       - in: path
+ *         name: noteName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the note
+ *     responses:
+ *       200:
+ *         description: Note deleted successfully
+ *       404:
+ *         description: Note not found
+ */
 app.delete('/notes/:noteName', (req, res) => {
     const notePath = path.join(option.cache, req.params.noteName);
 
@@ -82,6 +169,28 @@ app.delete('/notes/:noteName', (req, res) => {
     res.send('Note deleted');
 });
 
+/**
+ * @swagger
+ * /notes:
+ *   get:
+ *     summary: Get all notes
+ *     responses:
+ *       200:
+ *         description: List of notes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                     description: Note name
+ *                   text:
+ *                     type: string
+ *                     description: Note content
+ */
 app.get('/notes', (req, res) => {
     const files = fs.readdirSync(option.cache);
     const notes = files.map(fileName => {
@@ -97,6 +206,32 @@ app.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+/**
+ * @swagger
+ * /write:
+ *   post:
+ *     summary: Create a new note
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               note_name:
+ *                 type: string
+ *                 description: Name of the new note
+ *               note:
+ *                 type: string
+ *                 description: Content of the new note
+ *     responses:
+ *       201:
+ *         description: Note created successfully
+ *       400:
+ *         description: Note already exists
+ *       500:
+ *         description: Error creating note
+ */
 app.post('/write', upload.none(), (req, res) => {
     const noteName = req.body.note_name;
     const noteText = req.body.note;
@@ -113,6 +248,21 @@ app.post('/write', upload.none(), (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /UploadForm.html:
+ *   get:
+ *     summary: Serve the upload form HTML file
+ *     responses:
+ *       200:
+ *         description: HTML form retrieved successfully
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: HTML file not found
+ */
 app.get('/UploadForm.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'UploadForm.html'));
 });
